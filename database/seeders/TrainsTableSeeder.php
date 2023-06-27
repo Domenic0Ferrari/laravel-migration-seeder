d92cdaaf30800c023d5ae723f579fbe6e0ef069d
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-27 15:23:00 +0200

    add migration and seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+// importo la Model e il Faker
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
