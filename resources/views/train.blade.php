5ffa2c9dca831034c4941284e7be12950dea417a
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-27 15:05:51 +0200

    add header, navbar, train and base (.blade.php)

---
+++ resources/views/train.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.base')
+
+@section('contents')
+    <h2>I nostri treni</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sapiente error veniam minus sit voluptas modi odit veritatis, mollitia non dolores doloremque asperiores ducimus vitae assumenda est vel id cupiditate.</p>
+    {{-- <ul>
+        @foreach ($houses as $house)
+        <li>Casa:</li>
+        <li>{{ $house->address }}</li>
+        <li>{{ $house->rooms }}</li>
+        <li>{{ $house->reference }}</li>
+        <li>{{ $house->state }}</li>
+        @endforeach
+    </ul> --}}
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-        @vite('resources/js/app.js')
-    </head>
-    <body>
-        <h1 class="text-center">Ciao da vite</h1>
-        {{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
-    </body>
-</html>
+@extends('layouts.base')
+
+@section('contents')
+    <h2>Benvenuti nella nostra libereria di Treni!!</h2>
+@endsection
